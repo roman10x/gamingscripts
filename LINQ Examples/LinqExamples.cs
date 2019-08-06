1cd5a346b2abc53ed567ecd8cdea08be765743de
Francis <francis@engigames.ca>
2019-08-06 23:09:32 +0100

    Explain this: LINQ examples Updated Readme

---
+++ LINQ Examples/LinqExamples.cs
@@ -0,0 +1,102 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LinqExamples : MonoBehaviour
+{
+    //Object Initializer Syntax to initialize list of people
+    List<Person> personList = new List<Person>()
+    {
+        new Person(true, "Bob", "Roberts"),
+        new Person(true, "Bill", "Williams"),
+        new Person(true, "Bob", "Saginowski")
+    };
+
+    //Unity standard Start function
+    void Start()
+    {
+        //Normal foreach to set all person.isAlive to false
+        foreach (Person _person in personList)
+        {
+            _person.isAlive = false;
+        }
+
+        //Not Linq! Simple lambda and anonymous function equivalent to above foreach loop
+        personList.ForEach(_person => _person.isAlive = false);
+
+
+
+        //We Create new list to hold person objects with first name bob
+        List<Person> listOfBobs = new List<Person>();
+
+        //Iterate over the list, and add people with first name "Bob" to the listOfBobs
+        foreach (Person _person in personList)
+        {
+            if (_person.firstName == "Bob")
+            {
+                listOfBobs.Add(_person);
+            }
+        }
+
+
+        //Create a list using WHERE made only of people whose first name is "Bob"
+        List<Person> listOfBobsUsingWhere = personList.Where(_person => _person.firstName == "Bob").ToList();
+
+        //Filter out the person objects WHERE firstName "Bob" and isAlive is true.
+        List<Person> listOfAliveBobs = personList.Where(_person => _person.firstName == "Bob" && _person.isAlive == true).ToList();
+
+
+
+        //Returns true if ALL person objects in personList have isAlive set to true
+        bool areAllAlive = personList.All(_person => _person.isAlive);
+
+
+
+        //Returns true if ANY person objects in personList have isAlive set to true
+        bool areAnyAlive = personList.Any(_person => _person.isAlive);
+
+
+
+        //List created by selecting the first name of each person Object
+        List<string> firstNameList = personList.Select(_person => _person.firstName).ToList();
+
+
+
+        //Person List ordered by first name alphabetically
+        List<Person> orderedPersonList = personList.OrderBy(_person => _person.firstName).ToList();
+
+        //Using the same List
+        personList = personList.OrderBy(_person => _person.firstName).ToList();
+
+        //Person List ordered by descending first name alphabetically
+        List<Person> orderedByDescendingPersonList = personList.OrderByDescending(_person => _person.firstName).ToList();
+
+        //List OrderedBy SELECTED first names WHERE isAlive is true
+        List<string> orderedFirstNameListOfAlivePeople = 
+            personList.Where(_person => _person.isAlive).OrderBy(_person => _person.firstName).Select(_person => _person.firstName).ToList();
+    }
+
+    //Equivalent signature of anonymous function (person => _person.isAlive = false);
+    void SetIsAliveToFalse(Person _person)
+    {
+        _person.isAlive = false;
+    }
+}
+
+
+//Person class with constructor for each parameter
+public class Person
+{
+    public Person(bool _isAlive, string _firstName, string _lastName)
+    {
+        isAlive = _isAlive;
+        firstName = _firstName;
+        lastName = _lastName;
+    }
+
+    public bool isAlive;
+
+    public string firstName;
+    public string lastName;
+}
+
--- readme.md
+++ readme.md
@@ -8,6 +8,8 @@
 ### What is this?
 This repository is a compilation of source code used in various tutorials created by EngiGames. The purpose of these tutorials is to explain the basic concepts of common features found in games today. The code does not include the actual Unity project to ensure the package is suitable for all Unity versions. See the tutorial relative to the code folder for instructions on how to use it. 
 
+Included are also the links to code from the Explain This series
+
 
 
 | Date | Game | Tutorial | Description |
@@ -15,9 +17,5 @@ This repository is a compilation of source code used in various tutorials create
 | 12/18 | [__Cannon Game__](https://bitbucket.org/EngiGamesBitbucket/howtomakethatgame/src/master/FireBalls3D%20Example/) | [YouTube](https://www.youtube.com/watch?v=KGkQLHZC4bI) | A simple game where you fire a projectile from point A to point B using a pool of bullet objects.
 | 03/19 | [__Stickman Hook__](https://bitbucket.org/EngiGamesBitbucket/howtomakethatgame/src/master/Stickman%20Example/) | [YouTube](https://www.youtube.com/watch?v=1DF_Qm-Feyk) | A game where you hook to anchors to swing around levels and bounce off platforms to reach the end.
 | 06/19 | [__Archero__](https://bitbucket.org/EngiGamesBitbucket/howtomakethatgame/src/master/Archero%20Example/) | [YouTube](https://www.youtube.com/watch?v=7jzC4a9Nzes) | A game where you either moving, or shooting at hordes of enemies - Perfect for a state machine!
+| 08/06 | [__LINQ__](https://bitbucket.org/EngiGamesBitbucket/howtomakethatgame/src/master/LINQ%20Examples/) | [YouTube](TBD) | Explaining the basics of LINQ and how to use it in your game
 
-
-
-
-### About us
-__Visit our [Website](https://www.engigames.ca) or follow us on [Twitter](www.twitter.com/engigames) to stay up to date with the latest news.__
